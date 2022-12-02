16f640721bb4d6115362fa16e39e1fcb6d8f88e4
kerkazou <kerkazou.zakaria@gmail.com>
2022-12-02 11:26:57 +0100

    MAR-40 <Modification sur schema de categories>

--- server/models/categorieModal.js
+++ server/models/categorieModal.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const Categorie = mongoose.model(
   "Categorie",
   new mongoose.Schema({
-    name: String
+    name: String,
+    description: String,
   })
 );
 
