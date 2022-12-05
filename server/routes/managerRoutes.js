45bb8fb8017d377848e62c6ca007b22a2afdae73
zaiad <zaiadessahel3@gmail.com>
2022-12-05 09:41:43 +0100

    MAR-30 <terminé partie crudCategorie>

--- server/routes/managerRoutes.js
+++ server/routes/managerRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
-const authController = require('../controllers/userControllers/userControllers');
+const categorieControllers = require('../controllers/userControllers/categorieControllers');
 
 
 router.get('/me', (req, res)=>{ res.send('manager') });
+router.post('/add-categorie', categorieControllers.addCategorie);
+router.post('/findCategorie', categorieControllers.findCategorie);
+router.put('/updateCategorie/:id', categorieControllers.updateCategorie);
+router.delete('/deleteCategorie/:id', categorieControllers.deleteCategorie);
 
 
 module.exports = router;
