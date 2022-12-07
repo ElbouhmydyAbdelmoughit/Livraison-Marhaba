c5ed4eb786e72cf786c93555910de7a0f95d1b91
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-12-07 11:30:59 +0100

    changed routes and changed crud category to aggregation stage and add new folder in client for part add to cart

--- client/src/components/Livreur/Livreur.js
+++ client/src/components/Livreur/Livreur.js
@@ -38,8 +38,8 @@ export default function Livreur() {
           <input
             type="text"
             id="table-search-users"
-            className="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search htmlFor users"
+            className="block p-2 pl-8 w-40 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search For user"
           />
         </div>
       </div>
