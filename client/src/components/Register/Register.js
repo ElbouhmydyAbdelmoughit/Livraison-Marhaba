d26e787e22dcc2c3114056bac737446b5a99c820
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-12-10 15:06:59 +0100

    changed architecture redux

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -7,66 +7,132 @@ import axios from "axios";
 // import env from "react-dotenv";
 
 export default function Register() {
+  const [register, setRegister] = useState({
+    username: "",
+    email: "",
+    password: "",
+    cofirm_password: "",
+  });
 
-    const [register, setRegister] = useState({
-        username: '',
-        email: '',
-        password: '',
-        cofirm_password: ''
-    })
+  const onChange = (e) => {
+    const value = e.target.value;
+    setRegister({ ...register, [e.target.name]: value });
+  };
 
-    const onChange = (e) => {
-        const value = e.target.value;
-        setRegister({...register, [e.target.name]: value})
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/api/auth/register`, register)
+      .then((e) => {
+        if (e.data.message) {
+          Generator("success", e.data.message);
+          localStorage.setItem("email", e.data.email);
+          localStorage.setItem("password", e.data.password);
+          window.location = "/login";
+        } else Generator("error", e.data);
+      })
+      .catch(() => {
+        console.log("Error");
+      });
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        axios.post(`${process.env.REACT_APP_API_URL}/api/auth/register`, register)
-            .then(e=>{
-                if(e.data.message) {
-                    Generator('success', e.data.message)
-                    localStorage.setItem('email', e.data.email)
-                    localStorage.setItem('password', e.data.password)
-                    window.location = '/login'
-                }
-                else Generator('error', e.data)
-            })
-            .catch(()=>{ console.log('Error') })
-    }
-
-    return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-4xl font-bold text-center text-amber-500">Reset Password</h1>
-                        <form onSubmit={onSubmit} className="space-y-4 md:space-y-6">
-                            <div>
-                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                                <input type="text" value={register.username} onChange={onChange} name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="first & last name " />
-                            </div>
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="text" value={register.email} onChange={onChange} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" value={register.password} onChange={onChange} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" />
-                            </div>
-                            <div>
-                                <label htmlFor="cofirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cofirm Password</label>
-                                <input type="password" value={register.cofirm_password} onChange={onChange} name="cofirm_password" id="cofirm_password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" />
-                            </div>
-                            <button type="submit" className="w-full text-white bg-amber-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign up</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Have an account yet? <a href="/Login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</a>
-                            </p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <ToastContainer/>
-        </section>
-    );
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-4xl font-bold text-center text-amber-500">
+              Reset Password
+            </h1>
+            <form onSubmit={onSubmit} className="space-y-4 md:space-y-6">
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  value={register.username}
+                  onChange={onChange}
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="first & last name "
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="text"
+                  value={register.email}
+                  onChange={onChange}
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@company.com"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  value={register.password}
+                  onChange={onChange}
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="cofirm_password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Cofirm Password
+                </label>
+                <input
+                  type="password"
+                  value={register.cofirm_password}
+                  onChange={onChange}
+                  name="cofirm_password"
+                  id="cofirm_password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-amber-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign up
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Have an account yet?{" "}
+                <a
+                  href="/Login"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign in
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </section>
+  );
 }
