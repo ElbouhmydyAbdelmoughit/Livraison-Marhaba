f437524053a55292677ae4a16f6bfbbbf686b0f5
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-12-11 21:58:04 +0100

    MAR-19 <create store file and create configuration store>

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,8 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import cartSlice from "./redux/cartSlices";
+
+// const store = configureStore({
+//   reducer: cartSlice,
+// });
+
+// export default store;
