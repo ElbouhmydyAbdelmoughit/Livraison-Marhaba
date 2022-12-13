69ddd2706e56fae20287befc1a452962c8d3f3e6
safabalo <safaabalha58@gmail.com>
2022-12-13 14:41:42 +0100

    MAR-42 <message>

--- client/src/components/Client/Client.js
+++ client/src/components/Client/Client.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import Sidebar from '../Sidebar/Sidebar'
+import { MdOutlineDashboard} from "react-icons/md";
+import {AiOutlineShoppingCart, AiOutlineShopping} from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { BiLogOut } from "react-icons/bi";
+// import { Link } from "react-router-dom";
+import Product from './Product'
 export default function Client() {
+  const menus = [
+    { name: "dashboard", link: "/", icon: MdOutlineDashboard },
+    { name: "produit", link: "/", icon: AiOutlineShopping },
+    { name: "cart", link: "/", icon: AiOutlineShoppingCart },
+    { name: "parametre", link: "/", icon: FiSettings },
+    { name: "logout", link: "/", icon: BiLogOut, margin: true },
+  ];
   return (
-    <div>Client</div>
+    <div className="flex w-screen">
+    <Sidebar menus={menus}/>
+    <main className="w-full h-screen"> 
+      <Product/>
+    </main>
+  </div>
   )
 }
--- client/src/components/Manager/Manager.jsx
+++ client/src/components/Manager/Manager.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import Sidebar from './Sidebar'
+import Sidebar from '../Sidebar/Sidebar'
 import Livreur from '../Livreur/Livreur'
+import { MdOutlineDashboard, MdOutlineDeliveryDining } from "react-icons/md";
+import { BiLogOut, BiUserCircle } from "react-icons/bi";
 
 function Manager() {
+  const menus = [
+    { name: "Dashboard", link: "/statistique", icon: MdOutlineDashboard },
+    { name: "Commande", link: "/", icon: MdOutlineDeliveryDining },
+    { name: "Payement", link: "/", icon: MdOutlineDeliveryDining },
+    { name: "Users", link: "/", icon: BiUserCircle },
+    { name: "logout", link: "/", icon: BiLogOut, margin: true },
+];
   return (
     <div className="flex w-screen">
-      <Sidebar />
+      <Sidebar menus={menus}/>
       <main className="w-full h-screen"> 
         <Livreur/>
       </main>
