f0484eb6f27b1d8338eb7c652e542c88cb1a673c
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-12-15 09:55:13 +0100

    add cart component and create design for pannie

---
+++ client/src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+const Cart = () => {
+  return (
+    <div className="text-center">
+      <h2>Shopping Cart</h2>
+      <div className="text-center">
+        <p className="">Your cart is currently empty</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
