81871f677bf30008d8782edbc17413dcdecfbbf8
kerkazou <kerkazou.zakaria@gmail.com>
2022-12-18 15:41:42 +0100

    MAR-34 <Fonction logout>

--- client/src/components/Manager/Sidebar.jsx
+++ client/src/components/Manager/Sidebar.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import { MdOutlineDashboard, MdOutlineDeliveryDining } from "react-icons/md";
-import { BiLogOut, BiUserCircle } from "react-icons/bi";
+import { BiUserCircle } from "react-icons/bi";
 export default function SidebarM() {
     const menus = [
         { name: "Dashboard", link: "/statistique", icon: MdOutlineDashboard },
         { name: "Commande", link: "/", icon: MdOutlineDeliveryDining },
         { name: "Payement", link: "/", icon: MdOutlineDeliveryDining },
         { name: "Users", link: "/", icon: BiUserCircle },
-        { name: "logout", link: "/", icon: BiLogOut, margin: true },
     ];
   return (
     <Sidebar menus={menus}/>
