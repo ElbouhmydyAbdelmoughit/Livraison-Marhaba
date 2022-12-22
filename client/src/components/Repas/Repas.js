b116a4ea784be265e25ea6f5de36a7a2e3cae639
kerkazou <kerkazou.zakaria@gmail.com>
2022-12-22 14:29:35 +0100

    MAR-49 <Quelque modification sur les card des repas>

--- client/src/components/Repas/Repas.js
+++ client/src/components/Repas/Repas.js
@@ -70,21 +70,3 @@ export default function Repas() {
     </div >
   );
 }
-
-
-
-
-// <a key={p.id} href={p.href} className="pb-3 rounded shadow-md group" onClick={handleClick}>
-//               <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 h-72 xl:aspect-w-7 xl:aspect-h-8">
-//                 <img
-//                 src={process.env.REACT_APP_API_URL+'/'+p.image}
-//                 alt={p.image} className="object-cover object-center w-full h-full group-hover:opacity-75" />
-//               </div>
-//               <div className="flex justify-between px-3 mt-4">
-//                 <p className="mt-1 text-lg font-medium text-gray-900">{p.title}</p>
-//                 <BsCartPlus onClick={() => Send(p)} style={{fontSize:"1.7rem", cursor:"pointer"}} className="hover:text-amber-500"/>
-//               </div>
-//               {/* <p className="mt-1 text-sm">{p.categorie[0].name}</p> */}
-//               <p className="px-3 mt-1 text-sm text-gray-900">{p.description}</p>
-//               <p className="px-3 mt-1 text-lg font-medium text-gray-900">{p.price} DH</p>
-//             </a>
