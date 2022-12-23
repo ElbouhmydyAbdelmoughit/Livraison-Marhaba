dba44644e4c448053cc19bdd4caa34a27a7a8119
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-12-23 22:29:41 +0100

    create delete action in redux but not worked

--- client/src/redux/actions/action.js
+++ client/src/redux/actions/action.js
@@ -1,6 +1,15 @@
+/* Add Iteams In Card */
 export const ADD = (item) => {
   return {
       type: "ADD_CART",
       payload: item
+  }
+}
+
+/* Remove Iteams In Card */
+export const DLT = (_id) => {
+  return {
+      type: "RMV_CART",
+      payload: _id
   }
 }
