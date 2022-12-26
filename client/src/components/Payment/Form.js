e417f5d6bb0a81c74e716fccd750a50bee3025a2
safabalo <safaabalha58@gmail.com>
2022-12-26 16:03:43 +0100

    MAR-33 payment task completed

--- client/src/components/Payment/Form.js
+++ client/src/components/Payment/Form.js
@@ -1,21 +1,35 @@
 
 import React, { useState } from "react";
+import axios from "axios";
 
-import PayButton from "../Cart/PayButton";
 
 export default function Form() {
-  const cart = JSON.parse(localStorage.getItem("mealData"));
+
 
   const[phone,setPhone]= useState('')
   const[adress,setAdress]= useState('')
   const[city,setCity]= useState('')
   const[country,setCountry]= useState('')
   const[zip,setZip]= useState('')
-
+  // let [full,setFull]=useState('')
+  // setFull(full = (adress+' '+city+' '+country+' '+zip))
+  const mode = 'cash on delivery'
+  const client = localStorage.getItem('_id')
+  const meal = JSON.parse(localStorage.getItem('mealData'))
+  const price = meal[0].price
+  const data ={phone,adress,mode,client,price}
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    axios.post(`${process.env.REACT_APP_API_URL}/client/cash`,data)
+    .then((res)=>{
+      console.log(res.data)
+    })
+    .catch((err)=>{console.log(err.msg)})
+  }
 
   return (
     <div className="leading-loose">
-      <form className="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
+      <form className="max-w-xl m-4 p-10 bg-white rounded shadow-xl" onSubmit={handleSubmit}>
 
         <p className="text-amber-500 text-center text-2xl font-bold mb-6">Customer information</p>
         <div className="">
@@ -107,9 +121,8 @@ export default function Form() {
           />
         </div>
         <p className="mt-4 text-gray-800 font-medium">Payment information</p>
-        <div className="flex justify-between">
-          <button className="border px-4 py-1 text-white tracking-wider bg-gray-500 rounded hover:bg-white hover:text-gray-500 hover:border-gray-500">Cash on delivery</button>
-          <PayButton cartItems={cart} />
+        <div className="flex justify-center">
+          <button className="border px-4 py-1 text-white tracking-wider bg-amber-500 rounded hover:bg-white hover:text-amber-500 hover:border-amber-500">Cash on delivery</button>
         </div>
       </form>
     </div>
